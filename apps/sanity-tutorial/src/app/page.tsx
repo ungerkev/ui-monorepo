737a08659668b591904e69d1cdba5802d58f4d42
ungerkev <unger.kevin97@gmail.com>
2025-10-26 10:51:54 +0100

    display headline and fix cache issue

--- apps/sanity-tutorial/src/app/page.tsx
+++ apps/sanity-tutorial/src/app/page.tsx
@@ -15,6 +15,10 @@ export default async function Home() {
 
   return (
     <>
+      <div className="flex gap-1.5">
+        <h1>Headline: </h1>
+        <span className="font-bold">{data?.hero?.headline}</span>
+      </div>
       <Test />
     </>
   );
