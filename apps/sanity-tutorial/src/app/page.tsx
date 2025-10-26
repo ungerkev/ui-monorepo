3c24705fff9892bc95ea21658a5f476672b8dd52
ungerkev <unger.kevin97@gmail.com>
2025-10-26 15:02:47 +0100

    try to fix revalidation

--- apps/sanity-tutorial/src/app/page.tsx
+++ apps/sanity-tutorial/src/app/page.tsx
@@ -2,13 +2,19 @@ import { Test } from "@repo/shared/ui/test";
 import groq from "groq";
 import { sanityClient } from "@/sanity/lib/sanityClient";
 import { HomeDoc } from "@/sanity/types";
+import { sanityFetch } from "@/sanity/lib/live";
 
 export default async function Home() {
-  const data = await sanityClient.fetch<HomeDoc | undefined>(
+  /*const data = await sanityClient.fetch<HomeDoc | undefined>(
     groq`*[_type=="home"][0]{ hero{ ... } }`,
     {},
-    //{ cache: "force-cache", next: { tags: ["sanity"] } },
-  );
+    { cache: "force-cache", next: { tags: ["sanity"] } },
+  );*/
+
+  const { data } = await sanityFetch({
+    query: groq`*[_type=="home"][0]{ hero{ ... } }`,
+    tags: ["sanity"],
+  });
 
   console.log("data", data?.hero);
 
